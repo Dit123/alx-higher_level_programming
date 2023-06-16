a1b73ba97536fd548ffe6ff82c84c6da5fc7b916
Dit123 <debhaitulua@gmail.com>
2023-06-16 12:15:39 +0000

    all

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-0-select_states module
+2-my_filter_states module
 """
 import MySQLdb
 import sys
@@ -10,7 +10,8 @@ if __name__ == '__main__':
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("""SELECT * FROM states WHERE name LIKE BINARY '{}'
+            ORDER BY states.id ASC""".format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-0-select_states module
+4-cities_by_state module
 """
 import MySQLdb
 import sys
@@ -10,7 +10,9 @@ if __name__ == '__main__':
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id
+                ORDER BY cities.id ASC""")
     rows = cur.fetchall()
     for row in rows:
         print(row)
